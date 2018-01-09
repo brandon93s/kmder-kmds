107ab4bbd057ea4403b4945964186e0de80e2211
Brandon Smith <bsmith@evestment.com>
2018-01-08 23:43:16 -0500

    Improve cyprot and google commands

--- kmds/crypto.js
+++ kmds/crypto.js
@@ -3,6 +3,6 @@ module.exports = {
     url: "https://api.cryptonator.com/api/ticker/{0}-{1}",
     jp: "$.ticker",
     log: (Reporter, value) => {
-        Reporter.prettyJson(value)
+        Reporter.prettyJson(value[0])
     }
 }
