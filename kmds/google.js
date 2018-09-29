bf8d99cbd6431edebed14d572e4ab95bae34bbb9
Brandon Smith <brandon93s@gmail.com>
2018-09-29 00:17:21 -0400

    Update to const

--- kmds/google.js
+++ kmds/google.js
@@ -1,7 +1,7 @@
 const opn = require('opn')
 
 module.exports.run = async (args) => {
-    let query = args.join(" ")
+    const query = args.join(" ")
     await opn(`https://www.google.com/search?q=${encodeURIComponent(query)}`)
     return `Google search for "${query}" opened in default browser...`
 }
